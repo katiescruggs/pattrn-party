e517a4e784d1af68b51b58b377e3768f7d6aac89
katiescruggs <katie.e.scruggs@gmail.com>
2018-02-10 18:57:42 -0700

    Fix bug from jquery's .show()

--- scripts/header.js
+++ scripts/header.js
@@ -1,6 +1,6 @@
 $('.first-ul').on('click', () => {
   if ($(window).width() < 1203) {
-    $('.second-ul').toggle();
+    $('.second-ul').toggleClass('show');
     $('#header-btn').toggleClass('huge-margin');
   }
 });
